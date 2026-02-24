86b789bbfbfd5150cddbe73719ee884a34ae95cf
relllzi <165898813+relllzi@users.noreply.github.com>
2026-02-24 13:08:40 +0700

    fixed contact message sending

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -16,7 +16,7 @@ export default function ContactForm() {
     e.preventDefault();
     setStatus("Sending...");
     try {
-      const res = await fetch("http://localhost:5050/contact]", {
+      const res = await fetch("http://localhost:5050/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
